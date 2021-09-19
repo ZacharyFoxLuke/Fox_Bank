b7ecc16c3f2927df4fe01b727c885200bbce6b11
Zachary Fox Luke <72349944+ZacharyFoxLuke@users.noreply.github.com>
2021-09-19 14:05:50 -0400

    added a desktop and mobile navbar!

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 // ---------------------------------------- UNIVERSAL ---------------------------------------- //
 // ------------------------------------------------------------------------------------------- //
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+
+
+// ---------------------------------------- DESKTOP NAVBAR
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
     }
-}
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
+
+
+// ---------------------------------------- MOBILE NAVBAR
+const toggleButton = document.getElementsByClassName('fa-bars')[0]
+const mobileNavBar = document.getElementsByClassName('mobile-navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    mobileNavBar.classList.toggle('active')
+})
+
+
 
 // ---------------------------------------- FLICKITY ---------------------------------------- //
 // ------------------------------------------------------------------------------------------ //
